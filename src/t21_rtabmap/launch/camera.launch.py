a9ae188d757571250a5e9cda5525c77c5f489a5a
timofeenko-mf <timofeenko@ms-17.ru>
2025-07-21 22:15:59 +0300

    add rtabmap stack from spam; added D435 launch and yaml files; requires test and params check - nav.yaml wasnt changed

---
+++ src/t21_rtabmap/launch/camera.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+import os
+package_name='t21_rtabmap'
+def generate_launch_description():
+    # Путь к YAML-файлу параметров
+    default_params_file = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'camera.yaml'
+    )
+    
+    return LaunchDescription([
+        # Аргументы для переопределения
+        DeclareLaunchArgument('camera_name', default_value='camera'),
+        DeclareLaunchArgument('camera_namespace', default_value='camera'),
+        DeclareLaunchArgument('config_file', default_value=default_params_file),
+        
+        # Нода камеры
+        Node(
+            package='realsense2_camera',
+            executable='realsense2_camera_node',
+            name=LaunchConfiguration('camera_name'),
+            namespace=LaunchConfiguration('camera_namespace'),
+            parameters=[LaunchConfiguration('config_file')],
+            output='screen'
+        )
+    ])
