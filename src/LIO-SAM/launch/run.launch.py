cd82d6025f2a23d96bf1dd02e7d26939fa168d7f
qarol46 <qarol46ows@gmail.com>
2025-12-11 17:34:35 +0300

    worked pointcloud_to_laserscan and slam_toolbox only for build 2d map

--- src/LIO-SAM/launch/run.launch.py
+++ src/LIO-SAM/launch/run.launch.py
@@ -12,6 +12,7 @@ def generate_launch_description():
     parameter_file = LaunchConfiguration('params_file')
     xacro_path = os.path.join(get_package_share_directory('tracked_description'), 'urdf', 'tracked_robot.urdf.xacro')
     rviz_config_file = os.path.join(share_dir, 'config', 'rviz2.rviz')
+    slam_params_file = os.path.join(share_dir, 'config', 'mapper_params.yaml')
 
     params_declare = DeclareLaunchArgument(
         'params_file',
@@ -23,13 +24,13 @@ def generate_launch_description():
 
     return LaunchDescription([
         params_declare,
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     arguments='0.0 0.0 0.0 0.0 0.0 0.0 map odom'.split(' '),
-        #     parameters=[parameter_file],
-        #     output='screen'
-        #     ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments='0.0 0.0 0.0 0.0 0.0 0.0 map odom'.split(' '),
+            parameters=[parameter_file],
+            output='screen'
+            ),
         # Node(
         #     package='tf2_ros',
         #     executable='static_transform_publisher',
@@ -80,5 +81,47 @@ def generate_launch_description():
             name='rviz2',
             arguments=['-d', rviz_config_file],
             output='screen'
-        )
+        ),
+        Node(
+            package='pointcloud_to_laserscan',
+            executable='pointcloud_to_laserscan_node',
+            name='pointcloud_to_laserscan',
+            parameters=[{
+                'min_height': 0.05,      
+                'max_height': 0.5,
+                'angle_min': -3.14159,   
+                'angle_max': 3.14159,   
+                'angle_increment': 0.0087, 
+                'scan_time': 0.1,
+                'range_min': 0.8,
+                'range_max': 40.0,       
+                'use_inf': True,
+                'inf_epsilon': 1.0,
+                'target_frame': 'base_link',
+                'transform_tolerance': 0.5,   
+                'concurrency_level': 1,
+                # QoS for publisher
+                #'qos_overrides./scan.publisher.reliability': 'best_effort',
+                #'qos_overrides./scan.publisher.durability': 'volatile',
+                #'qos_overrides./scan.publisher.depth': 50,
+            }],
+            remappings=[
+                ('cloud_in', '/lio_sam/deskew/cloud_deskewed'),
+                ('scan', '/scan_velodyne')
+            ],
+            output='screen'
+        ),
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[slam_params_file],
+            remappings=[
+                ('scan', '/scan_velodyne'),
+                ('/odom', '/lio_sam/mapping/odometry'),
+                ('map', '/map'),
+                ('map_metadata', '/map_metadata')
+            ]
+        ),
     ])
